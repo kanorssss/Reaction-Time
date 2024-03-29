9acd14536b8b5b98528230812e120e3f17fcf9ff
Airon Jim <pingadairon@gmail.com>
2024-03-29 16:43:52 +0800

    testing

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -1,13 +1,32 @@
 <template>
     <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data(){
+        return{
+            rank:null,
+        }
+    },
+    mounted(){
+        if (this.score < 250){
+            this.rank = 'Ninja Fingers'
+        }else if(this.score < 400){
+            this.rank = 'Rapid Reflexis'
+        }else {
+            this.rank = 'Snail Pace'
+        }
+    }
 }
 </script>
 
 <style>
-
+.rank {
+    font-size: 1.4em;
+    color:aqua;
+    font-weight: bold ;
+}
 </style>
